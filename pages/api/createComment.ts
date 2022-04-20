03613c51446fca23b2273d93c45f642077626c57
Ana Tavares <ana.tavares@smartdata.science>
2022-04-20 16:39:08 -0300

    Added Comment Section

---
+++ pages/api/createComment.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import sanityClient from '@sanity/client'
+
+type Data = {
+  name: string
+}
+
+const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  useCdn: process.env.NODE_ENV === 'production',
+  apiVersion: '2021-08-31',
+  token: process.env.SANITY_API_TOKEN,
+}
+
+const client = sanityClient(config)
+
+export default  async function createComment(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { _id, name, email, comment } = JSON.parse(req.body)
+  try {
+    await client.create({
+      _type: 'comment',
+      post: {
+        _type: 'reference',
+        _ref: _id,
+      },
+      name,
+      email,
+      comment,
+    })
+  } catch (error) {
+    return res.status(500).json({ message: 'Cound not submit comment', error })
+  }
+  res.status(200).json({ message: 'Comment submitted' })
+}
